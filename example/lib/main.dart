b5735101146ee7194b66b2ccfdd4898f98714ea7
gzlock <gzlock88@gmail.com>
2020-04-24 06:31:51 +0800

    v1.0.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:focus_widget/focus_widget.dart';
 
@@ -40,17 +41,25 @@ class DemoLocalizations {
       'tips': 'When the FocusWidget has focus,\n'
           'Trigger the PointerDown event outside the FocusWidget area\n'
           'Will make FocusWidget lose focus\n'
-          'and trigger the FocusNode listener',
+          'and trigger the FocusNode listener\n',
+      'parameters': 'Parameters: \n'
+          '\t\t\tshowFocusArea: display a red rectangle it mean the focus area\n'
+          '\t\t\tjustHideKeyboard: tap outside of the input, just hide the keyboard\n',
       'address': 'Address',
       'name': 'name',
+      'justHide': 'Just hide the keyboard, keep it has focus'
     },
     'zh': {
       'tips': '当输入框获得焦点后，\n'
           '在FocusWidget域外触发PointerDown\n'
           '会让FocusWidget失去焦点'
           '并且触发FocusNode的listener',
+      'parameters': '参数：\n'
+          '\t\t\tshowFocusArea：显示一个淡红色方框高亮焦点区域\n'
+          '\t\t\tjustHideKeyboard：隐藏键盘后让输入框保持获得焦点的样子',
       'address': '地址',
       'name': '名称',
+      'justHide': '只隐藏键盘，保持焦点'
     },
   };
 
@@ -59,6 +68,10 @@ class DemoLocalizations {
   String get name => _localizedValues[locale]['name'];
 
   String get tips => _localizedValues[locale]['tips'];
+
+  String get justHide => _localizedValues[locale]['justHide'];
+
+  String get parameters => _localizedValues[locale]['parameters'];
 }
 
 class MyHomePage extends StatefulWidget {
@@ -71,11 +84,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final FocusNode _address = FocusNode(), _name = FocusNode();
+  GlobalKey<ScaffoldState> _scaffold = GlobalKey();
+  final TextEditingController _email = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final language = DemoLocalizations.of(context);
     return Scaffold(
+      key: _scaffold,
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -83,57 +99,123 @@ class _MyHomePageState extends State<MyHomePage> {
         child: SafeArea(
           child: ListView(
             padding: EdgeInsets.only(left: 10, right: 10),
-            children: [
-              FocusWidget.builder(
-                context,
-                (ctx, focusNode) => TextField(
-                  focusNode: focusNode,
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    hintText: 'Input',
-                    labelText: 'Input',
-                  ),
-                ),
-              )
-            ],
+            children: ListTile.divideTiles(
+                context: context,
+                tiles: List.generate(20, (i) {
+                  if (i != 5) {
+                    return ListTile(title: Text(i.toString()));
+                  }
+                  return FocusWidget.builder(
+                    context,
+                    showFocusArea: true,
+                    builder: (ctx, focusNode) => TextField(
+                      focusNode: focusNode,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        hintText: 'Input',
+                        labelText: 'Input',
+                      ),
+                    ),
+                  );
+                })).toList(),
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.only(left: 10, right: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 100,
-              child: Center(
-                child: Text(language.tips),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 100,
+            child: Center(
+              child: Text.rich(
+                TextSpan(
+                  children: [
+//                    TextSpan(text: language.tips),
+                    TextSpan(
+                        text: language.parameters,
+                        style: TextStyle(
+                          color: Colors.blue,
+                        )),
+                  ],
+                ),
+                style: TextStyle(fontSize: 18),
+                textWidthBasis: TextWidthBasis.longestLine,
               ),
             ),
-            Container(
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
               width: 100,
               child: FocusWidget(
+                showFocusArea: true,
                 focusNode: _address,
                 child: TextField(
                   focusNode: _address,
+                  decoration: InputDecoration(
+                      hintText: language.name, labelText: language.name),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: FractionallySizedBox(
+              widthFactor: 0.6,
+              child: FocusWidget(
+                focusNode: _name,
+                child: TextField(
+                  focusNode: _name,
                   decoration: InputDecoration(
                       hintText: language.address, labelText: language.address),
                 ),
               ),
             ),
-            FocusWidget(
-              focusNode: _name,
-              child: TextField(
-                focusNode: _name,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: FocusWidget.builder(
+              context,
+              justHideKeyboard: true,
+              builder: (context, FocusNode focusNode) => TextField(
+                focusNode: focusNode,
                 decoration: InputDecoration(
-                    hintText: language.name, labelText: language.name),
+                  hintText: language.justHide,
+                  labelText: 'FocusWidget.builder',
+                ),
               ),
             ),
-            SizedBox(
-              height: 1000,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: FocusWidget.builder(
+              context,
+              onLostFocus: (_) {
+                print('email is empty: ${_email.text.isEmpty}');
+                return _email.text.isNotEmpty;
+              },
+              builder: (context, FocusNode focusNode) {
+                return TextField(
+                  controller: _email,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    labelText: 'Email',
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            child: Text('Open Drawer'),
+            onPressed: () {
+              _scaffold.currentState.openDrawer();
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/focus_widget.dart
+++ lib/focus_widget.dart
@@ -1,26 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 typedef FocusNodeBuilder = Widget Function(
     BuildContext context, FocusNode focusNode);
+typedef OnLostFocus = bool Function(Widget widget);
 
 class FocusWidget extends StatefulWidget {
   final FocusNode focusNode;
   final Widget child;
+  final OnLostFocus onLostFocus;
+  final bool justHideKeyboard;
+  final bool showFocusArea;
 
   const FocusWidget({
     Key key,
-    this.focusNode,
-    this.child,
+    @required this.child,
+    @required this.focusNode,
+    this.justHideKeyboard = false,
+    this.showFocusArea = false,
+    this.onLostFocus,
   })  : assert(child != null && focusNode != null),
         super(key: key);
 
   @override
   FocusWidgetState createState() => FocusWidgetState();
 
-  static FocusWidget builder(BuildContext context, FocusNodeBuilder builder) {
+  static FocusWidget builder(
+    BuildContext context, {
+    @required FocusNodeBuilder builder,
+    justHideKeyboard = false,
+    showFocusArea = false,
+    OnLostFocus onLostFocus,
+  }) {
     final focusNode = FocusNode();
     return FocusWidget(
       focusNode: focusNode,
+      justHideKeyboard: justHideKeyboard,
+      showFocusArea: showFocusArea,
+      onLostFocus: onLostFocus,
       child: builder(
         context,
         focusNode,
@@ -31,17 +49,18 @@ class FocusWidget extends StatefulWidget {
 
 class FocusWidgetState extends State<FocusWidget> {
   OverlayEntry _overlayEntry;
-  Offset topLeft, bottomRight;
+  Rect rect;
 
   @override
   void initState() {
     super.initState();
-    widget.focusNode.addListener(_focusEvent);
+//    widget.focusNode.addListener(update);
+    widget.focusNode.addListener(update);
   }
 
   @override
   void dispose() {
-    widget.focusNode.removeListener(_focusEvent);
+    widget.focusNode.removeListener(update);
     super.dispose();
   }
 
@@ -49,49 +68,82 @@ class FocusWidgetState extends State<FocusWidget> {
     if (!widget.focusNode.hasFocus) return;
     final RenderBox renderBox = context.findRenderObject();
     final size = renderBox.size;
-    topLeft = renderBox.localToGlobal(Offset.zero);
-    bottomRight = topLeft + Offset(size.width, size.height);
+    final offset = renderBox.localToGlobal(Offset.zero);
+    rect = Rect.fromLTWH(offset.dx, offset.dy, size.width, size.height);
+    _createOverlay();
   }
 
-  void _focusEvent() {
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  void _createOverlay() {
+    _removeOverlay();
     if (widget.focusNode.hasFocus) {
-      update();
 //      print('focus ${_focusNode.hasFocus} \n'
 //          'topLeft:$topLeft \n'
 //          'bottomRight: $bottomRight');
+      bool canLostFocus = true;
+      if (widget.onLostFocus != null) {
+        canLostFocus = widget.onLostFocus(widget.child);
+      }
+      print('canLostFocus $canLostFocus');
+      final children = <Widget>[
+        Listener(
+          behavior: HitTestBehavior.translucent,
+          onPointerDown: (PointerDownEvent e) {
+            if (widget.onLostFocus != null &&
+                widget.onLostFocus(widget.child) == false) {
+              return;
+            }
+            if (rect.contains(e.position) == false) {
+              print('超出');
+              if (widget.justHideKeyboard) {
+                SystemChannels.textInput.invokeMethod('TextInput.hide');
+              } else {
+                widget.focusNode?.unfocus();
+                _removeOverlay();
+              }
+            }
+          },
+          child: canLostFocus
+              ? null
+              : Container(
+                  color: Colors.transparent,
+                ),
+        ),
+      ];
+      if (widget.showFocusArea) {
+        children.insert(
+            0,
+            Positioned.fromRect(
+              child: Container(
+                color: Colors.red.withOpacity(0.2),
+              ),
+              rect: rect,
+            ));
+      }
       _overlayEntry = new OverlayEntry(
         builder: (context) {
           return Stack(
-            children: [
-              Listener(
-                behavior: HitTestBehavior.translucent,
-                onPointerDown: (PointerDownEvent e) {
-//                  print('onPointerDown \n'
-//                      'dx ${e.position.dx} ${topLeft.dx} ${bottomRight.dx}\n'
-//                      'dy ${e.position.dy} ${topLeft.dy} ${bottomRight.dy}');
-                  final overX = e.position.dx <= topLeft.dx ||
-                      e.position.dx >= bottomRight.dx;
-                  final overY = e.position.dy <= topLeft.dy ||
-                      e.position.dy >= bottomRight.dy;
-                  // print('dx: $overX \ndy: $overY');
-                  if (overX || overY) {
-                    // print('超出');
-                    widget.focusNode?.unfocus();
-                  }
-                },
-              ),
-            ],
+            children: children,
           );
         },
       );
       Overlay.of(context).insert(_overlayEntry);
-    } else {
-      _overlayEntry?.remove();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return NotificationListener<LayoutChangedNotification>(
+      child: widget.child,
+      onNotification: (LayoutChangedNotification notification) {
+//        print('notification $notification');
+        update();
+        return true;
+      },
+    );
   }
 }
